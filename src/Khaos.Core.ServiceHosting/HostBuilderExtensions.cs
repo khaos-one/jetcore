1ed20fabb2924167773a500a03e890cd652af8c0
Egor Petenev <egor@petenev.me>
2019-10-12 16:06:17 +0300

    Add host builder logging configuration helper

--- src/Khaos.Core.ServiceHosting/HostBuilderExtensions.cs
+++ src/Khaos.Core.ServiceHosting/HostBuilderExtensions.cs
@@ -1,7 +1,11 @@
+using System;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
 using System.IO;
 using Khaos.Core.Configuration.Yaml;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Khaos.Core.ServiceHosting
 {
@@ -30,6 +34,26 @@ namespace Khaos.Core.ServiceHosting
                                 optional: true, reloadOnChange: false)                           
                             .AddYamlFile("appsettings.configmap.yml", optional: true)
                             .AddEnvironmentVariables();
+                    });
+        }
+
+        public static IHostBuilder WithDefaultLogging(
+            this IHostBuilder builder, 
+            Action<HostBuilderContext, ILoggingBuilder> configurator = null)
+        {
+            return builder
+                .ConfigureLogging(
+                    (context, loggingBuilder) =>
+                    {
+                        loggingBuilder
+                            .AddConfiguration(context.Configuration.GetSection("logging"));
+
+                        if (Debugger.IsAttached)
+                        {
+                            loggingBuilder.AddDebug();
+                        }
+                        
+                        configurator?.Invoke(context, loggingBuilder);
                     });
         }
     }
